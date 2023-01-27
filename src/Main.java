039ab4743e6dc48ff6f50e782357b7321841dc3a
AndreaScarsato <andrea.scarsato@gmail.com>
2023-01-27 12:38:22 +0100

    Inserimento delle parole di un testo in un ArrayList di stringhe

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,35 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    static void getParole (String s, ArrayList<String> t){
+/*    static void getParole (String s, ArrayList<String> t){
         int index = s.indexOf(" ");
+        int index_ = 0;
+        while (index == )
         String parola = s.substring(0, index);
 //        System.out.println(parola);
-    }
+        t.add(parola);
+    }*/
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in;
+        /*BufferedReader in;
         try {
             in = new BufferedReader(new FileReader("train.txt"));
         }catch (Exception e ){
             throw new RuntimeException();
-        }
+        }*/
+        Scanner in = new Scanner(new FileReader("train.txt"));
+        String s = in.next();
         ArrayList<String> testo = new ArrayList<>();
-        String s = in.readLine();
-        while (s != null){
-            getParole(s, testo);
+        testo.add(s);
+//        String s = in.readLine();
+        while (in.hasNext()){
+            s = in.next();
+            testo.add(s);
         }
+        System.out.println(testo);
     }
 }
